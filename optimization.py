a57b49a6b61548ed3e146f7ea6ae9a8b26e99a11
Boris Albar <b.albar@catie.fr>
2024-09-20 13:36:10 +0000

    Rewrite optimizer to support foreach and kahan summation

---
+++ optimization.py
@@ -0,0 +1,106 @@
+import torch
+import optimi
+
+from src.utils.adamw_scaled import AdamWScale
+
+from torch.optim.lr_scheduler import (
+    SequentialLR,
+    LinearLR,
+    CosineAnnealingLR,
+    ConstantLR
+)
+
+def create_optimizer(model, lr, betas, eps, weight_decay, kahan_sum=False):
+
+    no_decay = ["bias", "LayerNorm", "layernorm", "layer_norm", "ln"]
+
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+            "weight_decay": weight_decay,
+        },
+        {
+            "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+            "weight_decay": 0.0,
+        },
+    ]
+
+    optimizer = AdamWScale(
+        optimizer_grouped_parameters,
+        lr=lr,
+        betas=betas,
+        eps=eps,
+        weight_decay=weight_decay,
+        kahan_sum=kahan_sum,
+        foreach=True # Try to force foreach by default
+    )
+
+    return optimizer
+
+def create_cosine_scheduler(warmup_steps, warmup_ratio, num_training_steps, optimizer):
+
+    warmup_steps = warmup_steps if warmup_steps != 0 else int(num_training_steps * warmup_ratio)
+
+    if warmup_steps == 0:
+        lr_scheduler = CosineAnnealingLR(
+            optimizer,
+            T_max=num_training_steps - warmup_steps,
+            eta_min=1e-5,
+        )
+    else:
+        scheduler1 = LinearLR(
+            optimizer,
+            start_factor=0.5,
+            end_factor=1,
+            total_iters=warmup_steps,
+            last_epoch=-1,
+        )
+
+        scheduler2 = CosineAnnealingLR(
+            optimizer,
+            T_max=num_training_steps - warmup_steps,
+            eta_min=1e-5,
+        )
+
+        lr_scheduler = SequentialLR(
+            optimizer,
+            schedulers=[scheduler1, scheduler2],
+            milestones=[warmup_steps]
+        )
+
+    return lr_scheduler
+
+def create_wsd_scheduler(warmup_steps, warmup_ratio, num_training_steps, optimizer):
+
+    warmup_steps = warmup_steps if warmup_steps != 0 else int(num_training_steps * warmup_ratio)
+
+    scheduler1 = LinearLR(
+        optimizer,
+        start_factor=0.5,
+        end_factor=1,
+        total_iters=warmup_steps,
+        last_epoch=-1,
+    )
+
+    scheduler2 = ConstantLR(
+        optimizer,
+        factor=1.0,
+        total_iters=num_training_steps - 2*warmup_steps,
+        last_epoch=-1,
+    )
+
+    scheduler3 = LinearLR(
+        optimizer,
+        start_factor=1.0,
+        end_factor=0.5,
+        total_iters=warmup_steps,
+        last_epoch=-1,
+    )
+
+    lr_scheduler = SequentialLR(
+        optimizer,
+        schedulers=[scheduler1, scheduler2, scheduler3],
+        milestones=[warmup_steps, num_training_steps - warmup_steps]
+    )
+
+    return lr_scheduler
--- src/utils/adamw_scaled.py
+++ src/utils/adamw_scaled.py
@@ -1,8 +1,11 @@
 import torch
 import math
+
 from torch.optim import Optimizer
+from torch.optim.optimizer import _default_to_fused_or_foreach
+from torch.utils._foreach_utils import _group_tensors_by_device_and_dtype
 from typing import Iterable, Tuple
-from torch import nn
+from torch import nn, Tensor
 
 class AdamWScale(Optimizer):
     """
@@ -36,6 +39,8 @@ class AdamWScale(Optimizer):
         betas: Tuple[float, float] = (0.9, 0.999),
         eps: float = 1e-6,
         weight_decay: float = 0.0,
+        kahan_sum: bool = False,
+        foreach: bool = False,
         correct_bias: bool = True,
     ):
         if lr < 0.0:
@@ -46,13 +51,16 @@ class AdamWScale(Optimizer):
             raise ValueError(f"Invalid beta parameter: {betas[1]} - should be in [0.0, 1.0)")
         if not 0.0 <= eps:
             raise ValueError(f"Invalid epsilon value: {eps} - should be >= 0.0")
-        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
+
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, foreach=foreach, kahan_sum=kahan_sum, correct_bias=correct_bias)
+
         super().__init__(params, defaults)
 
     @staticmethod
     def _rms(tensor):
         return tensor.norm(2) / (tensor.numel() ** 0.5)
 
+    @torch.no_grad()
     def step(self, closure=None):
         """
         Performs a single optimization step.
@@ -65,55 +73,189 @@ class AdamWScale(Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            for p in group["params"]:
+            params, grads, exp_avgs, exp_avg_sqs, steps, kahan_comps = [], [], [], [], [], []
+
+            # Initialization
+            for p in group['params']:
                 if p.grad is None:
                     continue
-                grad = p.grad.data
-                if grad.is_sparse:
-                    raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
+
+                params.append(p)
+                if p.grad.is_sparse:
+                    raise RuntimeError('AdamWScale does not support sparse gradients')
+                grads.append(p.grad)
 
                 state = self.state[p]
-                beta1, beta2 = group["betas"]
 
                 # State initialization
-                if len(state) == 0:
-                    state["step"] = 0
-                    # Exponential moving average of gradient values
-                    state["exp_avg"] = torch.zeros_like(p.data)
-                    # Exponential moving average of squared gradient values
-                    state["exp_avg_sq"] = torch.zeros_like(p.data)
+                if "kahan_comp" not in state:
+                    state['step'] = torch.tensor(0, dtype=torch.int32, device=p.device)
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
 
-                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
+                    if group["kahan_sum"] and p.dtype in [torch.float16, torch.bfloat16]:
+                        state["kahan_comp"] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    else:
+                        state["kahan_comp"] = None
 
-                state["step"] += 1
+                exp_avgs.append(state['exp_avg'])
+                exp_avg_sqs.append(state['exp_avg_sq'])
+                kahan_comps.append(state["kahan_comp"])
+                steps.append(state["step"])
 
-                # Decay the first and second moment running average coefficient
-                # In-place operations to update the averages at the same time
-                exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1.0 - beta2)
-                denom = exp_avg_sq.sqrt().add_(group["eps"])
+            torch._foreach_add_(steps, 1)
 
-                step_size = group["lr"]
-                if group["correct_bias"]:  # No bias correction for Bert
-                    bias_correction1 = 1.0 - beta1 ** state["step"]
-                    bias_correction2 = 1.0 - beta2 ** state["step"]
-                    step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
-
-                # /Adapt Step from Adafactor
-                step_size = step_size * max(1e-3, self._rms(p.data))
-                # /Adapt Step from Adafactor
-
-                p.data.addcdiv_(exp_avg, denom, value=-step_size)
-
-                # Just adding the square of the weights to the loss function is *not*
-                # the correct way of using L2 regularization/weight decay with Adam,
-                # since that will interact with the m and v parameters in strange ways.
-                #
-                # Instead we want to decay the weights in a manner that doesn't interact
-                # with the m/v parameters. This is equivalent to adding the square
-                # of the weights to the loss with plain (non-momentum) SGD.
-                # Add weight decay at the end (fixed version)
-                if group["weight_decay"] > 0.0:
-                    p.data.add_(p.data, alpha=(-group["lr"] * group["weight_decay"]))
+            # AdamW step
+            if group["foreach"] and _default_to_fused_or_foreach(params, False, False):
+                self._foreach_adamwscaled(params,
+                                          grads,
+                                          exp_avgs,
+                                          exp_avg_sqs,
+                                          steps,
+                                          kahan_comps,
+                                          group["lr"],
+                                          group["betas"][0],
+                                          group["betas"][1],
+                                          group["weight_decay"],
+                                          group["eps"],
+                                          group["kahan_sum"],
+                                          group["correct_bias"])
+            else:
+                self._adamwscaled(params,
+                                  grads,
+                                  exp_avgs,
+                                  exp_avg_sqs,
+                                  steps,
+                                  kahan_comps,
+                                  group["lr"],
+                                  group["betas"][0],
+                                  group["betas"][1],
+                                  group["weight_decay"],
+                                  group["eps"],
+                                  group["kahan_sum"],
+                                  group["correct_bias"])
 
         return loss
+
+    def _adamwscaled(self,
+                    params: list[Tensor],
+                    grads: list[Tensor],
+                    exp_avgs: list[Tensor],
+                    exp_avg_sqs: list[Tensor],
+                    steps: list[Tensor],
+                    kahan_comps: list[Tensor],
+                    lr: float,
+                    beta1: float,
+                    beta2: float,
+                    weight_decay: float,
+                    eps: float,
+                    do_kahan_sum: bool,
+                    correct_bias: bool):
+
+        for i, p in enumerate(params):
+
+            exp_avg, exp_avg_sq, grad, step, kahan_comp = exp_avgs[i], exp_avg_sqs[i], grads[i], steps[i], kahan_comps[i]
+
+            # Decay the first and second moment running average coefficient
+            # In-place operations to update the averages at the same time
+            exp_avg.mul_(beta1).add_(grad, alpha=(1.0 - beta1))
+            exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=(1.0 - beta2))
+            denom = exp_avg_sq.sqrt().add_(eps)
+
+            step_size = lr
+            if correct_bias:  # No bias correction for Bert
+                bias_correction1 = 1.0 - beta1 ** step
+                bias_correction2 = 1.0 - beta2 ** step
+                step_size = step_size * math.sqrt(bias_correction2) / bias_correction1
+
+            # Adapt Step from Adafactor
+            step_size = step_size * max(1e-3, self._rms(p.data))
+
+            if do_kahan_sum:
+                # Adam step
+                kahan_comp.addcdiv_(exp_avg, denom, value=-step_size)
+
+                # update weights with kahan compensation using dev_grads as temp buffer
+                grad.copy_(p)
+                p.add_(kahan_comp)
+
+                # save error back to kahan compensation for next iteration
+                grad.sub_(p, alpha=1)
+                kahan_comp.add_(grad, alpha=1)
+            else:
+                p.addcdiv_(exp_avg, denom, value=-step_size)
+
+            # Just adding the square of the weights to the loss function is *not*
+            # the correct way of using L2 regularization/weight decay with Adam,
+            # since that will interact with the m and v parameters in strange ways.
+            #
+            # Instead we want to decay the weights in a manner that doesn't interact
+            # with the m/v parameters. This is equivalent to adding the square
+            # of the weights to the loss with plain (non-momentum) SGD.
+            # Add weight decay at the end (fixed version)
+            if weight_decay > 0.0:
+                p.add_(p, alpha=(-lr * weight_decay))
+
+    def _foreach_adamwscaled(self,
+                    params: list[Tensor],
+                    grads: list[Tensor],
+                    exp_avgs: list[Tensor],
+                    exp_avg_sqs: list[Tensor],
+                    steps: list[Tensor],
+                    kahan_comps: list[Tensor],
+                    lr: float,
+                    beta1: float,
+                    beta2: float,
+                    weight_decay: float,
+                    eps: float,
+                    do_kahan_sum: bool,
+                    correct_bias: bool):
+
+        grouped_tensors = _group_tensors_by_device_and_dtype([params, grads, exp_avgs, exp_avg_sqs, kahan_comps])
+
+        for (_, dtype), ((dev_params, dev_grads, dev_exp_avgs, dev_exp_avg_sqs, dev_kahan_comps), _) in grouped_tensors.items():
+            # Foreach implementation
+            torch._foreach_mul_(dev_exp_avgs, beta1)
+            torch._foreach_add_(dev_exp_avgs, dev_grads, alpha=1 - beta1)
+
+            torch._foreach_mul_(dev_exp_avg_sqs, beta2)
+            torch._foreach_addcmul_(dev_exp_avg_sqs, dev_grads, dev_grads, 1 - beta2)
+
+            # Compute denominator
+            torch._foreach_copy_(dev_grads, dev_exp_avg_sqs)
+            torch._foreach_sqrt_(dev_grads)
+            torch._foreach_add_(dev_grads, eps)
+
+            step_size = [torch.tensor(lr, dtype=torch.float32, device=p.device) for p in dev_params]
+
+            if correct_bias:
+                torch._foreach_mul_(step_size,
+                                   [torch.tensor((math.sqrt(1 - beta2 ** steps[i].item()) / (1 - beta1 ** steps[i].item()) ), dtype=torch.float32, device=p.device)
+                                        for i, p in enumerate(dev_params)])
+
+            # Adapt step size using RMS of parameters
+            rms_p = torch._foreach_norm(dev_params)
+            numel = [torch.tensor(math.sqrt(p.numel())) for p in dev_params]
+            rms_p = torch._foreach_div(rms_p, numel)
+
+            torch._foreach_mul_(step_size, torch._foreach_maximum(rms_p, 1e-3))
+            torch._foreach_div_(dev_grads, step_size)
+
+            # Update parameters
+            if do_kahan_sum:
+                # Adam step
+                torch._foreach_addcdiv_(dev_kahan_comps, dev_exp_avgs, dev_grads, value=-1)
+
+                # update weights with kahan compensation using dev_grads as temp buffer
+                torch._foreach_copy_(dev_grads, dev_params)
+                torch._foreach_add_(dev_params, dev_kahan_comps, alpha=1)
+
+                # save error back to kahan compensation for next iteration
+                torch._foreach_sub_(dev_grads, dev_params, alpha=1)
+                torch._foreach_add_(dev_kahan_comps, dev_grads, alpha=1)
+            else:
+                torch._foreach_addcdiv_(dev_params, dev_exp_avgs, dev_grads, value=-1)
+
+            # Weight decay
+            if weight_decay > 0.0:
+                torch._foreach_add_(dev_params, dev_params, alpha=-weight_decay * lr)
