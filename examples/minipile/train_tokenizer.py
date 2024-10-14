b6be57c781a3ce2c350ec6b515bb74ba7447d4a4
Boris Albar <b.albar@catie.fr>
2024-10-14 12:43:48 +0000

    Fix tokenizer minipile

--- examples/minipile/train_tokenizer.py
+++ examples/minipile/train_tokenizer.py
@@ -3,18 +3,16 @@ import datasets
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Sequence, Digits, Whitespace
+from tokenizers.pre_tokenizers import Sequence, Split
 
 from transformers import T5TokenizerFast
 
 # Load the dataset
 df = datasets.load_dataset("JeanKaddour/minipile", cache_dir="/mnt/storage3/hf_cache")["train"].select_columns(['text'])
 
-def get_training_corpus():
-    print("Number of steps : " + str(len(df) // 1000))
-    for start_idx in range(0, len(df) // 1000):
-        samples = df[start_idx * 1000 : start_idx + 1000]
-        yield samples["text"]
+def batch_iterator(dataset, batch_size=1000):
+    for batch in dataset.iter(batch_size=batch_size):
+        yield batch["raw_content"]
 
 special_tokens_dict = ["<cls>", "</s>", "<mask>", "<pad>", "<sep>", "<unk>"]
 
@@ -22,16 +20,16 @@ special_tokens_dict = ["<cls>", "</s>", "<mask>", "<pad>", "<sep>", "<unk>"]
 for i in range(256):
     special_tokens_dict.append("<extra_id_" + str(i) + ">")
 
-training_corpus = get_training_corpus()
 vocab_size = 32768
-pat_str = r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+"  # noqa: E501
+# Taken from Llama
+pat_str = r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+"
 
 # Train the tokenizer
 tokenizer = Tokenizer(BPE(unk_token="<unk>"))
 trainer = BpeTrainer(vocab_size=vocab_size, special_tokens=special_tokens_dict, max_token_length=20)
-pre_tokenizer = Sequence([Whitespace(), Digits(individual_digits=True)])
+pre_tokenizer = Sequence([Split(pat_str, behavior="isolated")])
 tokenizer.pre_tokenizer = pre_tokenizer
 
-tokenizer.train_from_iterator(training_corpus, trainer)
+tokenizer.train_from_iterator(batch_iterator(df), trainer)
 pretrained_tokenizer = T5TokenizerFast(tokenizer_object=tokenizer, clean_up_tokenization_spaces=False)
 pretrained_tokenizer.save_pretrained("tokenizer-fat5-minipile")
