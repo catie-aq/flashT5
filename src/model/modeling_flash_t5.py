3e4113416fbc3b831b632f4f625b6984878bd081
Boris Albar <b.albar@catie.fr>
2024-11-24 18:05:00 +0100

    Remove dtype conversion for FlashT5DenseAct

--- src/model/modeling_flash_t5.py
+++ src/model/modeling_flash_t5.py
@@ -122,12 +122,6 @@ class FlashT5DenseAct(nn.Module):
         hidden_states = self.wi(hidden_states)
         hidden_states = self.act(hidden_states)
         hidden_states = self.dropout(hidden_states)
-        if (
-            isinstance(self.wo.weight, torch.Tensor)
-            and hidden_states.dtype != self.wo.weight.dtype
-            and self.wo.weight.dtype != torch.int8
-        ):
-            hidden_states = hidden_states.to(self.wo.weight.dtype)
 
         return hidden_states
 
